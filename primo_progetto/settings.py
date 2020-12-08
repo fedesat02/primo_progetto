3db5d82f2cf3d1bd3aae6373feda86f720533cc1
fedesat02 <fedesat02@hotmail.com>
2020-12-08 16:31:06 +0100

    agg homepage

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -60,6 +60,7 @@ TEMPLATES = [
         'DIRS': [
             os.path.join(BASE_DIR, 'prima_app/templates'),
             os.path.join(BASE_DIR, 'seconda_app/templates'),
+            os.path.join(BASE_DIR, 'news/templates'),
             os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
