87a9b1c00060aeabffb6addd2d0f6126e294f52f
fedesat02 <fedesat02@hotmail.com>
2020-12-08 17:25:50 +0100

    configurato pannello admin

--- news/models.py
+++ news/models.py
@@ -6,6 +6,10 @@ class Giornalista(models.Model):
     nome = models.CharField(max_length=20)
     cognome = models.CharField(max_length=20)
 
+    class Meta:
+        verbose_name = 'Giornalista'
+        verbose_name_plural = 'Giornalisti'
+
     def __str__(self):
         return self.nome + " " + self.cognome
     
@@ -14,6 +18,10 @@ class Articolo(models.Model):
     titolo = models.CharField(max_length=100)
     contenuto = models.TextField()
     giornalista = models.ForeignKey(Giornalista, on_delete=models.CASCADE, related_name="articoli")
+
+    class Meta:
+        verbose_name = 'Articolo'
+        verbose_name_plural = 'Articoli'
 
     def __str__(self):
         return self.titolo
