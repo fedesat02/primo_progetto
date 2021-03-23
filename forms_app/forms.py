79827f0fa94c279f606a1009ebe161dd2558f2f4
fedesat02 <fedesat02@hotmail.com>
2021-03-23 13:38:37 +0100

    controllo testo breve

--- forms_app/forms.py
+++ forms_app/forms.py
@@ -1,8 +1,23 @@
 from django import forms
-from django.forms import fields
+from django.core.exceptions import ValidationError
+from django.forms import fields, widgets
 from .models import Contatto
 
 class FormContatto(forms.ModelForm):
     class Meta:
         model = Contatto
         fields = "__all__"
+        widgets = {
+            'nome': forms.TextInput(attrs={'placeholder': 'Compila questo campo', 'class': 'form-control'}),
+            'cognome': forms.TextInput(attrs={'placeholder': 'Compila questo campo', 'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Compila questo campo', 'class': 'form-control'}),
+            'contenuto': forms.Textarea(attrs={'placeholder': 'Area testuale scrivi pure', 'class': 'form-control'})
+        }
+    
+    def clean_contenuto(self): #nome funzione: clean_nomecampodavalidare
+        dati = self.cleaned_data["contenuto"]
+        if "parola" in dati: #parola non è ammesso
+            raise ValidationError("Il contenuto inserito viola le norme del sito!")
+        if len(dati)<20:
+            raise ValidationError("Il contenuto inserito è troppo breve")
+        return dati
