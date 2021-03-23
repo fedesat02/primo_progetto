a68aa48525da938925b447ddca864f36e3821f16
fedesat02 <fedesat02@hotmail.com>
2021-03-23 13:25:00 +0100

    salvare dati inseriti attraverso form nel database

--- forms_app/forms.py
+++ forms_app/forms.py
@@ -1,6 +1,8 @@
 from django import forms
-class FormContatto(forms.Form):
-    nome = forms.CharField()
-    cognome = forms.CharField()
-    email = forms.EmailField()
-    contenuto = forms.CharField(widget=forms.Textarea(attrs={"placeholder": "Area Testuale! Scrivi pure!"}))
+from django.forms import fields
+from .models import Contatto
+
+class FormContatto(forms.ModelForm):
+    class Meta:
+        model = Contatto
+        fields = "__all__"
